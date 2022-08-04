11d285b26c9a217d2295d427d9e6efcd3e7409c6
as-android-developer <asatarpk@gmail.com>
2022-08-04 12:45:02 +0500

    add required dependencies

---
+++ app/src/main/java/com/mywebsite/insalesproductinfoapp/App.kt
@@ -0,0 +1,12 @@
+package com.mywebsite.insalesproductinfoapp
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
---
+++ app/src/main/java/com/mywebsite/insalesproductinfoapp/view/activities/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.mywebsite.insalesproductinfoapp.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity :  AppCompatActivity() {
+
+
+    companion object{
+
+    }
+
+}
